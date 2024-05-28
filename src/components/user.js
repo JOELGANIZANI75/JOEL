5bda32b7c9d6bd5159c2b775d1a4278615347922
JOELGANIZANI75 <167243130+JOELGANIZANI75@users.noreply.github.com>
2024-05-28 02:17:40 +0200

    Update user.js

--- src/components/user.js
+++ src/components/user.js
@@ -2,9 +2,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const UserContext = createContext();
 
-export const useUser = () => {
-  return useContext(UserContext);
-};
+export const useUser = () => useContext(UserContext);
 
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
