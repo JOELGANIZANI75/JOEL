39b6b487f4cbeac015eccc12a61aca0f92090f1e
JOELGANIZANI75 <167243130+JOELGANIZANI75@users.noreply.github.com>
2024-06-07 21:16:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './components/user'; // Use named import
 import { NotificationsProvider } from './components/Alert';
+import { HostelProvider } from './HostelContext';
 
 ReactDOM.render(
   <UserProvider>
+   <HostelProvider>
     <NotificationsProvider>
       <App />
     </NotificationsProvider>
+  </HostelProvider>
   </UserProvider>,
   document.getElementById('root')
 );
