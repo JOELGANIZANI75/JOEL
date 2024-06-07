4614172db353be8e119afd02a58eb5312b7f30df
JOELGANIZANI75 <167243130+JOELGANIZANI75@users.noreply.github.com>
2024-06-07 21:59:34 +0200

    Update Hostel.js

--- src/components/Hostel.js
+++ src/components/Hostel.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useUser } from './user';
 
-
 const Hostel = ({ hostel }) => {
   const [showDetails, setShowDetails] = useState(false);
   const navigate = useNavigate();
   const { currentUser } = useUser();
+
   const handleBookNow = () => {
     if (!currentUser) {
-      navigate('/Login'); // Use navigate directly instead of returning JSX
+      navigate('/Login');
     } else {
       navigate('/BookingPage', { state: { hostel } });
     }
@@ -21,17 +21,26 @@ const Hostel = ({ hostel }) => {
 
   return (
     <div className="hostel shadow-lg rounded-lg overflow-hidden bg-white">
-      <img src={hostel.image} alt={hostel.name} className="w-full h-40 object-cover" />
+      <img src={hostel.images[0]} alt={hostel.name} className="w-full h-40 object-cover" />
       <div className="p-4">
         <h2 className="text-xl font-semibold">{hostel.name}</h2>
         {showDetails && (
           <div className="mt-2">
-            <p className="text-gray-600">Rent: {hostel.rent}</p>
-            <p className="text-gray-600">Date Uploaded: {hostel.dateUploaded}</p>
-            <p className="text-gray-600">Distance from Campus: {hostel.distance} km</p>
-            <p className="text-gray-600">Rooms Available: {hostel.roomsAvailable}</p>
-            <p className="text-gray-600">Gender: {hostel.gender}</p>
-            <p className="text-gray-600">Landlord Contact: {hostel.landlord}</p>
+            <p className="text-gray-600">Hostel Name: {hostel.name}</p>
+            <p className="text-gray-600">Room Type: {hostel.roomType}</p>
+            <p className="text-gray-600">Amount of Rooms: {hostel.amountofrooms}</p>
+            <p className="text-gray-600">Amount (MWK): {hostel.amount}</p>
+            <p className="text-gray-600">Directions to Campus: {hostel.distance}</p>
+            <div className="mt-2 grid grid-cols-3 gap-2">
+              {hostel.images.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  alt={`Hostel Image ${index + 1}`}
+                  className="w-full h-24 object-cover"
+                />
+              ))}
+            </div>
           </div>
         )}
         <button onClick={toggleDetails} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg mt-2 mr-2">
