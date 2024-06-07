9c9c97687e04d770e44c0ef07f5221c6b365d277
JOELGANIZANI75 <167243130+JOELGANIZANI75@users.noreply.github.com>
2024-06-07 22:40:20 +0200

    Update HostelList.js

--- src/components/HostelList.js
+++ src/components/HostelList.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import Hostel from './Hostel';
+import { useHostels } from './HostelContext';
 
-const HostelList = ({ hostels, onViewDetails }) => {
+const HostelList = ({ onViewDetails }) => {
+  const { hostels, isLoading } = useHostels();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (hostels.length === 0) {
+    return <div>No hostels available</div>;
+  }
+
   return (
     <div className="hostel-list grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-      {hostels.map((hostel, index) => (
-        <Hostel key={index} hostel={hostel} onViewDetails={onViewDetails} />
+      {hostels.map((hostel) => (
+        <Hostel key={hostel.id} hostel={hostel} onViewDetails={onViewDetails} />
       ))}
     </div>
   );
